2489e56ad44545c5021273689c8784f22a263191
Declan Warn <declan.warn@gmail.com>
2020-10-11 02:58:42 +1100

    Show relevant post information on feed

--- src/containers/Feed.js
+++ src/containers/Feed.js
@@ -7,19 +7,32 @@ const withLoader = promise => {
   promise.then(() => spinner.remove());
 }
 
+const showDateTime = published => {
+  const timestamp = Number(published) * 1000;
+  const date = new Date(timestamp);
+  return date.toLocaleString();
+};
+
 export default (api, goto) => {
+  const feed = create("div", { id: "feed" });
+
   const loadPosts = async () => {
     const { posts } = await api.user.feed();
-    document.getElementById("feed").append(
-      create("ul", undefined, posts.map(post =>
-        create("li", { textContent: post.meta.description_text })
+    feed.append(
+      create("ol", { className: "post-list" }, posts.map(post =>
+        create("li", { className: "post" }, [
+          create("img", { src: `data:image/png;base64,${post.src}` }),
+          create("span", { textContent: `${post.meta.description_text}` }),
+          create("span", { textContent: `${post.meta.author}` }),
+          create("span", { textContent: `${showDateTime(post.meta.published)}` }),
+          create("span", { textContent: `${post.meta.likes.length} like(s)` }),
+          create("span", { textContent: `${post.comments.length} comment(s)` }),
+        ])
       ))
     );
   };
 
   withLoader(loadPosts());
 
-  return (
-    create("div", { id: "feed" })
-  );
+  return feed;
 }
